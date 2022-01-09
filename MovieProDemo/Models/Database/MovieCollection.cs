95de8cbac6153fda81a566ee1c45f67722382e2d
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-01-08 23:52:08 -0500

    Movie Collection Properties
    Added class properties

--- MovieProDemo/Models/Database/MovieCollection.cs
+++ MovieProDemo/Models/Database/MovieCollection.cs
@@ -7,5 +7,13 @@ namespace MovieProDemo.Models.Database
 {
     public class MovieCollection
     {
+        public int Id { get; set; }
+        public int CollectionId { get; set; }
+        public int MovieId { get; set; }
+
+        public int Order { get; set; }
+
+        public Collection Collection { get; set; }
+        public Movie Movie { get; set; }
     }
 }
